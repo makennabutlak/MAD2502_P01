bf4149a6bada618d414731e34f0bf7b8f2c5947e
gabbypolito <g.polito@ufl.edu>
2025-02-18 14:42:43 -0500

    Added left_endpoint

--- riemann.py
+++ riemann.py
@@ -1,4 +1,13 @@
 import numpy as np
+
+def left_endpoint(x_vals: np.ndarray, func: np.ufunc) -> float:
+    sum_left_endpoint = 0
+    a = 0
+    b = 1
+    while x_vals[b]<=x_vals.size:
+        sum_left_endpoint += func(x_vals[a])*(x_vals[b]-x_vals[a])
+    return sum_left_endpoint
+    
 def trapezoid(x_vals: np.ndarray, func: np.ufunc) -> float:
     dx = np.diff(x_vals)
     return np.sum((func(x_vals[:-1]) + func(x_vals[1:])) * dx / 2)
