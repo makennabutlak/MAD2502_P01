fb9ceadcfffe7e52cdad01279b9315ce52ca4538
SolomiiaIvanovska <ivanovska.s@ufl.edu>
2025-02-18 17:49:13 -0500

    Update riemann.py
    Simpson function

--- riemann.py
+++ riemann.py
@@ -11,3 +11,12 @@ def left_endpoint(x_vals: np.ndarray, func: np.ufunc) -> float:
 def trapezoid(x_vals: np.ndarray, func: np.ufunc) -> float:
     dx = np.diff(x_vals)
     return np.sum((func(x_vals[:-1]) + func(x_vals[1:])) * dx / 2)
+
+def simpson(x_vals: np.ndarray, func: np.ufunc) -> float:
+    dx = np.diff(x_vals)  
+    n = len(x_vals) - 1
+    if n % 2 == 1: 
+        raise ValueError("The number of intervals must be even for Simpson's rule.")
+    return (dx[0] / 3) * np.sum(
+        func(x_vals[:-1:2]) + 4 * func(x_vals[1::2]) + func(x_vals[2::2])
+    )
