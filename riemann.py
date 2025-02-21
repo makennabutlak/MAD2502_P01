02acde31d255cf7c47deb22423b637bfe941e186
makennabutlak <makennabutlak@gmail.com>
2025-02-21 17:53:19 -0500

    changing simposons rule so that it runs correctly

--- riemann.py
+++ riemann.py
@@ -13,7 +13,6 @@ def simpson(x_vals: np.ndarray, func: np.ufunc) -> float:
     dx = np.diff(x_vals)
     n = len(x_vals) - 1
     if n % 2 == 1:
-        raise ValueError("The number of intervals must be even for Simpson's rule.")
-    return (dx[0] / 3) * np.sum(
-        func(x_vals[:-1:2]) + 4 * func(x_vals[1::2]) + func(x_vals[2::2])
-    )
+        return trapezoid(x_vals, func)
+    dx = (x_vals[-1]-x_vals[0])/n
+    return (dx/3)* (func(x_vals[0])+4* np.sum(func(x_vals[1:-1:2]))+2 * np.sum(func(x_vals[2:-2:2]))+ func(x_vals[-1]))
