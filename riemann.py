94e355d25e28f0140f731d9aa73604b3561dfb58
SolomiiaIvanovska <ivanovska.s@ufl.edu>
2025-02-21 22:09:45 -0500

    Update riemann.py
    Fixed Simpson function

--- riemann.py
+++ riemann.py
@@ -1,8 +1,8 @@
 import numpy as np
 
 def left_endpoint(x_vals: np.array, func: np.ufunc)-> float:
-    difference= np.diff(x_vals)
-    return np.sum(func(x_vals[:-1])*difference)
+    dx= np.diff(x_vals)
+    return np.sum(func(x_vals[:-1])*dx)
 
 def trapezoid(x_vals: np.ndarray, func: np.ufunc) -> float:
     dx = np.diff(x_vals)
@@ -10,9 +10,16 @@ def trapezoid(x_vals: np.ndarray, func: np.ufunc) -> float:
 
 
 def simpson(x_vals: np.ndarray, func: np.ufunc) -> float:
-    dx = np.diff(x_vals)
+    
     n = len(x_vals) - 1
     if n % 2 == 1:
-        return trapezoid(x_vals, func)
+        x_vals = x_vals[:-1]
+        n-=1
+        
     dx = (x_vals[-1]-x_vals[0])/n
-    return (dx/3)* (func(x_vals[0])+4* np.sum(func(x_vals[1:-1:2]))+2 * np.sum(func(x_vals[2:-2:2]))+ func(x_vals[-1]))
+    return (dx/3)* 
+            func(x_vals[0])
+            +4 * np.sum(func(x_vals[1:-1:2]))
+            +2 * np.sum(func(x_vals[2:-2:2]))
+            + func(x_vals[-1])
+)
