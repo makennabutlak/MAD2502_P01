3b2a25a82bc7f911b27fa29dc1be0b9cc06d1e26
gabbypolito <g.polito@ufl.edu>
2025-02-26 16:29:03 -0500

    Update barycentric.py
    Added comments and docstring to get_barycentric_coordinates and get_cartesian_coordinates

--- barycentric.py
+++ barycentric.py
@@ -1,6 +1,17 @@
 import numpy as np
 def get_barycentric_coordinates(triangle_coordinates: np.array, point_coordinates: np.array) -> np.array:
-    #Solomiia Ivanovska coded 
+    '''
+    Calculates the barycentric coordinates of P given a point P=(x,y) and a triange with verticies A1=(x1,y1), A2=(x2,y2), and A3=(x3,y3).
+
+    Parameters:
+    triangle_coordinates - the 3 coordinates of the triangle given as a 2-by-3 numpy array
+    point_coordinates - the point P as a 1d numpy array
+
+    Returns:
+    Barycentric coordinates as a 1d numpy array
+    '''
+    
+    # Verticies of triangle
     x1 = triangle_coordinates[0, 0]
     x2 = triangle_coordinates[0, 1]
     x3 = triangle_coordinates[0, 2]
@@ -8,10 +19,11 @@ def get_barycentric_coordinates(triangle_coordinates: np.array, point_coordinate
     y2 = triangle_coordinates[1, 1]
     y3 = triangle_coordinates[1, 2]
 
+    # Point P
     x = point_coordinates[0]
     y = point_coordinates[1]
 
-    # got this by solving for each element of the barycentric coordinates with te system
+    # got this by solving for each element of the barycentric coordinates with the system
     barycentric_1 = ((y2 - y3) * (x - x3) + (x3 - x2) * (y - y3)) / ((y2 - y3) * (x1 - x3) + (x3 - x2) * (y1 - y3))
     barycentric_2 = ((y3 - y1) * (x - x3) + (x1 - x3) * (y - y3)) / ((y2 - y3) * (x1 - x3) + (x3 - x2) * (y1 - y3))
     barycentric_3 = 1 - barycentric_1 - barycentric_2
@@ -19,6 +31,18 @@ def get_barycentric_coordinates(triangle_coordinates: np.array, point_coordinate
     return [barycentric_1, barycentric_2, barycentric_3]
 
 def get_cartesian_coordinates(triangle_coordinates: np.array, barycentric_coordinates: np.array) -> np.array:
+    '''
+    Calculates the cartesian coordinates of a triange with verticies A1=(x1,y1), A2=(x2,y2), and A3=(x3,y3) provided the barycentric_coordinates.
+
+    Parameters:
+    triangle_coordinates - the 3 coordinates of the triangle given as a 2-by-3 numpy array
+    barycentric_coordinates - the barycentric coordinates as a 1d numpy array
+
+    Returns:
+    Cartesian coordinates as a 1d numpy array in the form (x,y)
+    '''
+
+    # Verticies of triangle
     x1 = triangle_coordinates[0, 0]
     x2 = triangle_coordinates[0, 1]
     x3 = triangle_coordinates[0, 2]
@@ -26,10 +50,12 @@ def get_cartesian_coordinates(triangle_coordinates: np.array, barycentric_coordi
     y2 = triangle_coordinates[1, 1]
     y3 = triangle_coordinates[1, 2]
 
+    # Barycentric coordinates
     b1 = barycentric_coordinates[0]
     b2 = barycentric_coordinates[1]
     b3 = barycentric_coordinates[2]
-
+    
+    # Calculation of point using system
     x = b1 * x1 + b2 * x2 + b3 * x3
     y = b1 * y1 + b2 * y2 + b3 * y3
 
