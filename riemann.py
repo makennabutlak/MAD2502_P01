ee220a011314783b1db552c6607909696b13ccca
makennabutlak <makennabutlak@gmail.com>
2025-02-26 16:38:01 -0500

    added doc strings for left endpoint and trapezoid

--- riemann.py
+++ riemann.py
@@ -1,11 +1,15 @@
 import numpy as np
 
 def left_endpoint(x_vals: np.array, func: np.ufunc)-> float:
+    #takes the differences of all of the x values (representing the width, dx) in the array 
     dx= np.diff(x_vals)
+    #returns the sum of the difference in x values (dx) by x plugged into the equation (f(x)) stopping before the last indexed value since it is a left endpoint integral 
     return np.sum(func(x_vals[:-1])*dx)
 
 def trapezoid(x_vals: np.ndarray, func: np.ufunc) -> float:
+    #takes the difference of all of the x values in the array (representing dx)
     dx = np.diff(x_vals)
+    #returns the trapezoidal integral by finding the sum of left handed x values plus the right handed x values multiplied by the difference in x values and then divided by 2 since that is the formula for a trapezoid 
     return np.sum((func(x_vals[:-1]) + func(x_vals[1:])) * dx / 2)
 
 
